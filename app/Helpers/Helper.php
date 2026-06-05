ba099e350076ddb0602a7a8b7be6ca9187d6c514
apachertr180navneeet <navneet@brenin.co>
2026-06-05 12:03:39 +0530

    code update

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\User;
+use DB, Auth, File, Mail;
+use Carbon\Carbon;
+
+
+class Helper
+{
+   
+    public static function admin(){
+        $admin = User::where('id',1)->first();
+        return $admin;
+    }
+
+    public static function slug($table, $name)
+    {
+        $slug = str_replace(' ', '-', $name);
+        $slug = strtolower($slug);
+        $i = 1;
+        while ($i > 0) {
+            $check_slug = DB::table($table)->where('slug', $slug)->first();
+            if($check_slug) {
+                $slug = str_replace(' ', '-', $name) . '-' . $i;
+                $slug = strtolower($slug);
+                $i++;
+                continue;
+            }else{
+                break;
+            }
+        }
+
+        return $slug;
+    }
+
+    public static function slugUpdate($table, $name,$id)
+    {
+        $slug = str_replace(' ', '-', $name);
+        $slug = strtolower($slug);
+        $i = 1;
+        while ($i > 0) {
+            $check_slug = DB::table($table)->where('slug', $slug)->where('id','!=',$id)->first();
+            if($check_slug) {
+                $slug = str_replace(' ', '-', $name) . '-' . $i;
+                $slug = strtolower($slug);
+                $i++;
+                continue;
+            }else{
+                break;
+            }
+        }
+
+        return $slug;
+    }
+
+    public static function cleanImage($string)
+    {
+        $string = str_replace(' ', '-', $string);
+        return preg_replace('/[^A-Za-z0-9.\-]/', '', $string);
+    }
+
+
+    public static function userDetail($user_id)
+    {
+        $user_detail = User::find($user_id);
+        return $user_detail;
+    }
+
+   
+
+    public static function urlValidation(){
+        $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/';
+        return $regex;
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Web\HomeController;
+use App\Http\Controllers\Admin\AdminAuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', [HomeController::class, 'index'])->name('/');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::name('admin.')->prefix('admin')->group(function () {
+    Route::get('/', [AdminAuthController::class, 'index']);
+
+    Route::get('login', [AdminAuthController::class, 'login'])->name('login');
+
+    Route::post('login', [AdminAuthController::class, 'postLogin'])->name('login.post');
+
+    Route::get('forget-password', [AdminAuthController::class, 'showForgetPasswordForm'])->name('forget.password.get');
+
+    Route::post('forget-password', [AdminAuthController::class, 'submitForgetPasswordForm'])->name('forget.password.post');
+
+    Route::get('reset-password/{token}', [AdminAuthController::class, 'showResetPasswordForm'])->name('reset.password.get');
+
+    Route::post('reset-password', [AdminAuthController::class, 'submitResetPasswordForm'])->name('reset.password.post');
+
+    Route::middleware(['admin'])->group(function () {
+    	Route::get('dashboard', [AdminAuthController::class, 'adminDashboard'])->name('dashboard');
+
+        Route::get('change-password', [AdminAuthController::class, 'changePassword'])->name('change.password');
+
+        Route::post('update-password', [AdminAuthController::class, 'updatePassword'])->name('update.password');
+
+        Route::get('logout', [AdminAuthController::class, 'logout'])->name('logout');
+
+        Route::get('profile', [AdminAuthController::class, 'adminProfile'])->name('profile');
+
+        Route::post('profile', [AdminAuthController::class, 'updateAdminProfile'])->name('update.profile');
+
+    });
+
+});
+
+Route::middleware(['auth'])->group(function () {
+
+});
+
+
+
